0ccc33f2c87cb4591f889f4599ad9a08394fdbf7
Geo <gjsj@discente.ifpe.edu.br>
2025-08-30 09:20:44 -0300

    adding routes

--- routes/router.js
+++ routes/router.js
@@ -1,4 +1,8 @@
 import { express } from "express";
+import { appointment } from "./AppointmentController";
+import { doctorController } from "./DoctorController";
+import { pacientController } from "./PacientController";
+import { prescriptionController } from "./PrescriptionController";
 
 let router = express.Router();
 
@@ -8,5 +12,10 @@ router.get(
         res.status(200).json({message: "Ok!"});
     }
 );
+
+router.use("/", appointment )
+router.use("/", doctorController )
+router.use("/", pacientController )
+router.use("/", prescriptionController )
 
 export default router;
